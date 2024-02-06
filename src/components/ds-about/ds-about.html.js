82645a6a784fc61e6b4d46058ae8680046ec59fc
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-02-06 16:14:26 +0100

    Markup the about component

---
+++ src/components/ds-about/ds-about.html.js
@@ -0,0 +1,12 @@
+// Create new template.
+export const template = document.createElement('template')
+
+// Structure template.
+template.innerHTML = `
+<h3 class="about-title">Developer 079</h3>
+<p class="about-text">Hey there! I'm Dlovan, a web developer with a growing passion for iOS development. My coding journey started with a simple idea, leading me to explore online resources and university courses.</p>
+<p class="about-text">I'm not your typical developer; I'm a problem solver with a creative twist. I see programming as something organic, rather than mechanical, a way to bring ideas to life. Currently, I'm deeply involved in a project aiming to connect people based on their shared interests — forming unique digital tribes.</p>
+<p class="about-text">Swift and SwiftUI are my languages of choice. The elegance and simplicity of Swift resonate with me, making it my go-to tool for crafting intuitive user experiences.</p>
+<p class="about-text">Beyond coding, I find joy in contemplating life's progression and diving into philosophical discussions. Humility and honesty are my guiding principles in both personal and professional pursuits.</p>
+<p class="about-text">Let's embark on this coding adventure together and create something extraordinary!</p>
+`
