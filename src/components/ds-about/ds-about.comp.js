486f245342487da4a40a56e3d53c1032902358d6
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-02-07 16:47:30 +0100

    Remove HTML template for ds-about

--- src/components/ds-about/ds-about.comp.js
+++ src/components/ds-about/ds-about.comp.js
@@ -3,7 +3,6 @@
  * @module ds-about
  */
 
-import { template as markupTemplate } from './ds-about.html.js'
 import { template as styleTemplate } from './ds-about.css.js'
 
 /**
@@ -32,7 +31,6 @@ class extends HTMLElement {
     // Attach shadow DOM tree to this custom element, and
     // append templates to its shadow root.
     this.attachShadow({ mode: 'open' })
-    this.shadowRoot.append(markupTemplate.content.cloneNode(true))
     this.shadowRoot.append(styleTemplate.content.cloneNode(true))
 
     // Default values.
