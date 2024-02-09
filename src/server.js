ae144709b61af07be67fae092c96f6b9371c3785
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-02-09 15:34:52 +0100

    Serve static files, register routes, parse form data

--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
 import express from 'express'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { router } from './routes/router.js'
 
 const app = express()
 const port = process.env.PORT || 5050
+
+// Get the full directory path of this module.
+const directoryPath = dirname(fileURLToPath(import.meta.url))
+
+// Serve static files.
+app.use(express.static(join(directoryPath, '..', 'public')))
+// Parse requests of the content-type application/x-www-form-urlencoded.
+// Populates the request object with a body object (req.body).
+app.use(express.urlencoded({ extended: false }))
+
+// Register routes.
+app.use('/', router)
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
