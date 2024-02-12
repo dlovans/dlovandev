38c25444152a98916d84b5a6048b667c8666dc3b
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-02-12 17:45:16 +0100

    Remove eventObj as parameter and argument since events don't propagate out of shadow DOM boundaries

--- public/js/components/ds-contact/ds-contact.comp.js
+++ public/js/components/ds-contact/ds-contact.comp.js
@@ -71,7 +71,7 @@ customElements.define('ds-contact',
 
       // Register event handlers.
       this.#form.addEventListener('submit', (event) => this.#formSubmission(event))
-      this.#form.addEventListener('click', (event) => this.#indicateFields(event))
+      this.#form.addEventListener('click', () => this.#indicateFields())
       this.#form.addEventListener('input', (event) => this.#greenIndicatorFields(event))
     }
 
@@ -173,9 +173,8 @@ customElements.define('ds-contact',
     /**
      * Event handler indicating fields to be filled out. Utilizes bubbling.
      *
-     * @param {object} eventObj - The event object.
      */
-    #indicateFields (eventObj) {
+    #indicateFields () {
       for (const input of this.#inputFields) {
         if (input.classList.contains('success') || input.classList.contains('error')) {
           return
