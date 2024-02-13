fb3de681761db58623bca482ede11da69eb48653
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-02-13 16:31:10 +0100

    Update routing and HTTP method

--- src/routes/mailRouter.js
+++ src/routes/mailRouter.js
@@ -9,4 +9,4 @@ import { MailController } from '../controllers/MailController.js'
 const controller = new MailController()
 export const router = express.Router()
 
-router.get('/', async (req, res, next) => { controller.indexPost(req, res, next )})
+router.post('/', async (req, res, next) => { controller.validateData(req, res, next )})
--- src/routes/router.js
+++ src/routes/router.js
@@ -5,7 +5,9 @@
 
 import express from 'express'
 import { router as homeRouter } from './homeRouter.js'
+import { router as mailRouter } from './mailRouter.js'
 
 export const router = express.Router()
 
 router.use('/', homeRouter)
+router.use('/smtp', mailRouter)
