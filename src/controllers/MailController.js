f876852191ee12f0396db04cabdb53e898576981
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-02-14 01:42:44 +0100

    Define method for sending message over SMTP

--- src/controllers/MailController.js
+++ src/controllers/MailController.js
@@ -30,4 +30,52 @@ export class MailController {
       res.status(400).json({ "messageStatus": false, "message": "Fill out all fields." })
     }
   }
+
+  /**
+   * Sends the mail to admin.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async sendMail (req, res, next) {
+    try {
+      const { flname, email, message } = req.body
+
+      // Create a nodemailer transport object using SMTP.
+      const transporter = nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE_PROVIDER,
+        secure: true,
+        auth: {
+          user: process.env.USER,
+          pass: process.env.PASS
+        }
+      })
+
+      // The HTML content.
+      const markupContent = `
+        <html>
+        <head>
+          <title>Message from ${flname.trim()}</title>
+        </head>
+        <body>
+          <h1>${flname.trim()}:</h1>
+          <p>${message}</p>
+        </body>
+      </html>
+      `
+
+      // Send the message over SMTP.
+      await transporter.sendMail({
+        from: email.trim(),
+        to: process.env.USER,
+        subject: `${flname.trim()} requests audience`,
+        html: markupContent
+      })
+    } catch (error) {
+      error.status = 500
+      error.message = 'Error: failed to send message over SMTP.'
+      next(error)
+    }
+  }
 }
