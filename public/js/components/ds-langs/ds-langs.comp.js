f2c3e71a8acbf4237801de79c7e098206e5c9604
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-02-22 01:11:01 +0100

    Update indentation

--- public/js/components/ds-langs/ds-langs.comp.js
+++ public/js/components/ds-langs/ds-langs.comp.js
@@ -6,75 +6,75 @@
 import { template as styleTemplate } from './ds-langs.css.js'
 
 customElements.define('ds-langs',
-/**
- * Anonymous class representing the ds-langs custom element.
- */
-class extends HTMLElement {
   /**
-   * Relative source to the image.
+   * Anonymous class representing the ds-langs custom element.
    */
-  #imgSource
+  class extends HTMLElement {
+    /**
+     * Relative source to the image.
+     */
+    #imgSource
 
-  /**
-   * Initializes new instance.
-   */
-  constructor () {
-    // Attach shadow DOM tree to this custom element, and
-    // append templates to its shadow root.
-    this.attachShadow({ mode: 'open' })
-    this.shadowRoot.append(styleTemplate.content.cloneNode(true))
+    /**
+     * Initializes new instance.
+     */
+    constructor () {
+      // Attach shadow DOM tree to this custom element, and
+      // append templates to its shadow root.
+      this.attachShadow({ mode: 'open' })
+      this.shadowRoot.append(styleTemplate.content.cloneNode(true))
 
-    // Initialize imgSources field.
-    this.#imgSource = this.getAttribute('lang-img')
-  }
+      // Initialize imgSources field.
+      this.#imgSource = this.getAttribute('lang-img')
+    }
 
-  /**
-   * Invoked after the custom element is inserted into the DOM.
-   */
-  connectedCallback () {
-    this.#createImageTag(this.#imgSource)
-  }
-
-  /**
-   * Observes specified attributes for changes.
-   * 
-   * @returns {String[]} - An array of attributes.
-   */
-  static get observedAttributes () {
-    return ['lang-img']
-  }
-
-  /**
-   * Invoked when an observed attribute value changes.
-   * 
-   * @param {string} name - Name of the changed attribute.
-   * @param {string} oldValue - Attribute value before the change.
-   * @param {string} newValue - Attribute value after the change.
-   */
-  attributeChangedCallback (name, oldValue, newValue) {
-    if (name === 'lang-img' && oldValue !== newValue) {
-      this.#imgSource = newValue
+    /**
+     * Invoked after the custom element is inserted into the DOM.
+     */
+    connectedCallback () {
       this.#createImageTag(this.#imgSource)
     }
-  }
 
-  /**
-   * Creates image tag and appends it to the shadow root.
-   *
-   * @param {string} relativeSource - The relative path to the image source.
-   */
-  #createImageTag (relativeSource) {
-    // If img object exists, mutate the src attribute.
-    const imageObject = this.shadowRoot.querySelector('img')
-    if (imageObject) {
-      img.src = relativeSource
-    } else {
-      // Create new image tag.
-      const img = document.createElement('img')
-      img.src = imgSource
-      img.classList.add('lang-comp')
-      this.shadowRoot.append(img)
+    /**
+     * Observes specified attributes for changes.
+     * 
+     * @returns {String[]} - An array of attributes.
+     */
+    static get observedAttributes () {
+      return ['lang-img']
+    }
+
+    /**
+     * Invoked when an observed attribute value changes.
+     * 
+     * @param {string} name - Name of the changed attribute.
+     * @param {string} oldValue - Attribute value before the change.
+     * @param {string} newValue - Attribute value after the change.
+     */
+    attributeChangedCallback (name, oldValue, newValue) {
+      if (name === 'lang-img' && oldValue !== newValue) {
+        this.#imgSource = newValue
+        this.#createImageTag(this.#imgSource)
+      }
+    }
+
+    /**
+     * Creates image tag and appends it to the shadow root.
+     *
+     * @param {string} relativeSource - The relative path to the image source.
+     */
+    #createImageTag (relativeSource) {
+      // If img object exists, mutate the src attribute.
+      const imageObject = this.shadowRoot.querySelector('img')
+      if (imageObject) {
+        img.src = relativeSource
+      } else {
+        // Create new image tag.
+        const img = document.createElement('img')
+        img.src = imgSource
+        img.classList.add('lang-comp')
+        this.shadowRoot.append(img)
+      }
     }
   }
-}
 )
