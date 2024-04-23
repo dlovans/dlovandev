caae91b5bd44c0e84b451e2694214897b4d243a5
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-04-23 20:44:48 +0200

    Add some styling to ds-title

---
+++ public/js/components/ds-title/ds-title.css.js
@@ -0,0 +1,12 @@
+// Create template object.
+export const template = document.createElement('template')
+
+// Add styling for rendering.
+template.innerHTML = `
+  <style>
+    h2 {
+      font-size: 1.2rem;
+      white-space: nowrap;
+    }
+  </style>
+`
