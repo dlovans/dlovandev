bf240bbc7081bbba17e3caeee2b83aa28b6338e4
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-05-03 01:25:31 +0200

    Add expand wrapper to modal feature

--- public/js/components/ds-wrapper/ds-wrapper.css.js
+++ public/js/components/ds-wrapper/ds-wrapper.css.js
@@ -12,10 +12,29 @@ template.innerHTML = `
   }
 
   :host {
+    position: relative;
     overflow: hidden;
     border-radius: 15px;
     height: 100%;
     background-color: #242f36;
+  }
+
+  .expand-icon-wrapper {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    opacity: 0;
+    background-color: #ffffffbe;
+    transition: all 400ms ease;
+    z-index: 2;
+    cursor: pointer;
+  }
+
+  :host(:hover) .expand-icon-wrapper {
+    opacity: 1;
   }
 </style>
 `
