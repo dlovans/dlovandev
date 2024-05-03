0e60ebfafaa3aca86c10cf126bbe960b53e46f64
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-05-03 19:54:20 +0200

    Declare markup for ds-slogan web component

---
+++ public/js/components/ds-slogan/ds-slogan.html.js
@@ -0,0 +1,8 @@
+// Create template object.
+export const template = document.createElement('template')
+
+// Populate template.
+template.innerHTML = `
+  <h4 part="ds-slogan-text"></h4>
+  <button part="ds-slogan-button"></button>
+`
