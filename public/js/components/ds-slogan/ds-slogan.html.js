a42ad38d1a2a9586f40403f5e62f7a8206fe75c6
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-05-08 00:55:45 +0200

    Update ds-slogan markup

--- public/js/components/ds-slogan/ds-slogan.html.js
+++ public/js/components/ds-slogan/ds-slogan.html.js
@@ -3,6 +3,7 @@ export const template = document.createElement('template')
 
 // Populate template.
 template.innerHTML = `
-  <h4 part="ds-slogan-text"></h4>
-  <button part="ds-slogan-button"></button>
+  <h4 part="ds-slogan-text">
+    <button part="ds-slogan-button">Contact Me!</button>
+  </h4>
 `
