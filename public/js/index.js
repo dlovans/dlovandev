d45537be9504893f4e5ae834f47d107b8a5d3838
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-05-13 23:29:29 +0200

    Move script tags to import statements

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+import './components/ds-slogan/ds-slogan.comp.js'
+import './components/ds-persona/ds-persona.comp.js'
+import './components/ds-text/ds-text.comp.js'
+import './components/ds-svg-wrap/ds-svg-wrap.comp.js'
+import './components/ds-trait/ds-trait.comp.js'
+import './components/ds-wrapper/ds-wrapper.comp.js'
+import './components/ds-contact/ds-contact.comp.js'
