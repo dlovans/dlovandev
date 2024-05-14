e7db3a05954fe981fa190af7972fe7e730b197cf
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-05-14 13:41:00 +0200

    Define ds-app web component

---
+++ public/js/components/ds-app/ds-app.comp.js
@@ -0,0 +1,41 @@
+/**
+ * @description This module represents the ds-app web component.
+ * @module ds-app
+ */
+
+import { template as markupTemplate } from './ds-app.html.js'
+
+/**
+ * Define custome element ds-app.
+ */
+customElements.define('ds-app',
+    /**
+     * Anonymous class representing ds-app custom element.
+     * @extends HTMLElement
+     */
+    class extends HTMLElement {
+        /**
+         * Reference to ds-contact custom element.
+         */
+        #contactComponentRef
+
+        /**
+         * Creates an instance of this class.
+         */
+        constructor() {
+            super()
+            // Attach shadow DOM to this custom element,
+            // and append templates to its shadow root.
+            this.attachShadow({ mode: 'open' })
+            this.shadowRoot.append(markupTemplate.content.cloneNode(true))
+
+            // Get reference.
+            this.#contactComponentRef = this.shadowRoot.querySelector('ds-contact')
+            // Register event listener.
+            this.addEventListener('ds-trigger-focus', () => {
+                this.#triggerContactFocus()
+            })
+
+        }
+    }
+)
