f6a2670a1cbbf1f1c3b76d91a9ea060ac57698f9
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-05-27 16:07:04 +0200

    Remove import statement and markup

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,6 @@
 import './components/ds-app/ds-app.comp.js'
 
 import './components/ds-slogan/ds-slogan.comp.js'
-import './components/ds-persona/ds-persona.comp.js'
 import './components/ds-text/ds-text.comp.js'
 import './components/ds-svg-wrap/ds-svg-wrap.comp.js'
 import './components/ds-trait/ds-trait.comp.js'
