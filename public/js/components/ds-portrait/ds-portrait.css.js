72dc0bb36dfa24bc375b97174f0a285c96d375d8
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-05-29 17:46:11 +0200

    Style ds-portrait component

---
+++ public/js/components/ds-portrait/ds-portrait.css.js
@@ -0,0 +1,13 @@
+// Create template object.
+export const template = document.createElement('template')
+
+// Style the component.
+template.innerHTML = `
+    <style>
+        :host {
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: cover;
+        }
+    </style>
+`
