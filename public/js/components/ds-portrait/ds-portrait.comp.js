ca57adb2dbd61a41e64875e18c570caa6cf69b1c
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-05-29 13:32:30 +0200

    Define ds-portrait web component

---
+++ public/js/components/ds-portrait/ds-portrait.comp.js
@@ -0,0 +1,66 @@
+/**
+ * @description This module represents the ds-portrait web component.
+ * @module ds-portrait
+ */
+
+import { template as styleTemplate } from './ds-portrait.css.js'
+import {template as markupTemplate} from "../ds-app/ds-app.html.js";
+
+/**
+ * Define custom element ds-portrait.
+ */
+customElements.define('ds-portrait',
+    /**
+     * Anonymous class representing ds-portrait custom element.
+     * @extends HTMLElement
+     */
+    class extends HTMLElement {
+        /**
+         * The relative path of the portrait image source.
+         */
+        #imageSource
+
+        /**
+         * Creates an instance of this class.
+         */
+        constructor() {
+            super()
+
+            // Attach shadow DOM to this custom element,
+            // and append templates to its shadow root.
+            this.attachShadow({ mode: 'open' })
+            this.shadowRoot.append(markupTemplate.content.cloneNode(true))
+        }
+
+        /**
+         * Invoked after this custom element is inserted into the DOM.
+         */
+        connectedCallback() {
+            this.#setPortrait()
+        }
+
+        /**
+         * Specifies the attributes to be observed for changes.
+         *
+         * @returns {string[]} - The attributes to be observed.
+         */
+        static get observedAttributes() {
+            return ['ds-portrait-source']
+        }
+
+        /**
+         * Invoked when an observed attribute value changes.
+         *
+         * @param {string} name - Name of the changed attribute.
+         * @param {string} oldValue - Attribute value before the change.
+         * @param {string} newValue - Attribute value after the change.
+         */
+        attributeChangedCallback(name, oldValue, newValue) {
+            if (name === 'ds-portrait-source' && oldValue !== newValue) {
+                this.#setPortrait()
+            }
+        }
+
+    }
+
+)
