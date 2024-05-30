cefc7ec55af9d1c0ccd0e79bee59be0bd1941c45
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-05-30 21:43:59 +0200

    Define method that renders project items

--- public/js/components/ds-modal/ds-modal.comp.js
+++ public/js/components/ds-modal/ds-modal.comp.js
@@ -13,6 +13,16 @@ customElements.define('ds-modal',
      */
     class extends HTMLElement {
         /**
+         * Reference to the h2 object in shadow DOM.
+         */
+        #mainHeadingRef
+
+        /**
+         * Reference to the main content view container.
+         */
+        #mainContentRef
+
+        /**
          * Initializes an instance of this class.
          */
         constructor() {
@@ -23,13 +33,66 @@ customElements.define('ds-modal',
             this.attachShadow({ mode: 'open' })
             this.shadowRoot.append(markupTemplate.content.cloneNode(true))
 
+            // Get references to shadow DOM children.
+            this.#mainHeadingRef = this.shadowRoot.querySelector('.modal-title')
+            this.#mainContentRef = this.shadowRoot.querySelector('.project-view-container')
+
             // Register event handlers.
             this.addEventListener('ds-modal-projects', (event) => this.#renderProjects(event))
-            this.addEventListener('ds-modal-techs', (event) => this.renderTechs(event))
+            // this.addEventListener('ds-modal-techs', (event) => this.renderTechs(event))
         }
 
+        /**
+         * Renders each project item in the event detail object.
+         *
+         * @param {object} eventObj - The event object with project details.
+         */
         #renderProjects(eventObj) {
-            console.log(eventObj.detail)
+            const dataCollection = Array.from(eventObj.detail.data)
+            console.log(dataCollection)
+
+            this.#mainContentRef.innerHTML = ''
+            this.#mainHeadingRef.textContent = ''
+
+
+            for (const projectItem of dataCollection) {
+                const wrapper = document.createElement('div')
+                wrapper.classList.add('modal-project-wrapper')
+
+                const radioButton = document.createElement('input')
+                radioButton.setAttribute('type', 'radio')
+                radioButton.setAttribute('value', projectItem.projectKey)
+                if (projectItem.clicked) {
+                    radioButton.setAttribute('checked', '')
+                }
+
+                const projectHeader = document.createElement('h3')
+                projectHeader.textContent = projectItem.projectTitle
+                projectHeader.classList.add('modal-project-title')
+
+                const projectImage = document.createElement('div')
+                projectImage.style.backgroundImage = `url(${projectItem.projectScreenshot})`
+                projectImage.classList.add('modal-project-image')
+
+                const projectRepoURL = document.createElement('a')
+                const projectLiveURL = document.createElement('a')
+                projectRepoURL.href = projectItem.repoURL
+                projectLiveURL.href = projectItem.liveURL
+
+                const projectDescriptionCollection = projectItem.projectDescription.split('&#13')
+                const projectDescriptionWrapper = document.createElement('div')
+                projectDescriptionWrapper.classList.add('modal-project-description-wrapper')
+                for (const paragraph of projectDescriptionCollection) {
+                    const paragraphObject = document.createElement('p')
+                    paragraphObject.textContent = paragraph
+                    projectDescriptionWrapper.append(paragraphObject)
+                }
+
+                wrapper.append(projectHeader, projectImage, projectRepoURL, projectLiveURL, projectDescriptionWrapper)
+
+                this.#mainContentRef.append(wrapper)
+                this.#mainHeadingRef.textContent = 'Projects'
+            }
         }
     }
     )
