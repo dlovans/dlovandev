7326978ea1aabf1ed523151c4302975f3c549288
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-05-30 21:46:44 +0200

    Create style template for ds-modal

---
+++ public/js/components/ds-modal/ds-modal.css.js
@@ -0,0 +1,11 @@
+// Create template object.
+export const template = document.createElement('template')
+
+// Style the component.
+template.innerHTML = `
+    <style>
+        :host {
+            position: absolute;
+        }
+    </style>
+`
