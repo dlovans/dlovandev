8923f06651ba4b63c84b64b14ecaf5fb65b671e5
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-06-03 21:31:58 +0200

    Update ds-modal markup

--- public/js/components/ds-modal/ds-modal.html.js
+++ public/js/components/ds-modal/ds-modal.html.js
@@ -8,8 +8,7 @@ template.innerHTML = `
             <h2 class="modal-title">Aquity</h2>
             <button id="close-btn">X</button>
         </nav>
-        <div class="projects-view-container"></div>
-        <div class="techs-view-container"></div>
+        <div class="view-container"></div>
         <div class="radio-button-control"></div>
     </div>
     <span class="invisible-overlay"></span>
